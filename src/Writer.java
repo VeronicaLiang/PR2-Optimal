9406cb20c133b7c6ecb5dfb09dead388a549bdde
youbehappy <youbehappy@qq.com>
2015-11-20 16:37:33 -0500

    write

--- src/Writer.java
+++ src/Writer.java
@@ -4,8 +4,16 @@ public class Writer {
 		
 	}
 	
-	public int run () {
+	public int run (String add, Processor pro, int currentCycle) {
 		// run write operation, and return the number of finishing cycle
+		//if the operation is miss or hit
+				boolean isHitorMiss = Util.hitOrMiss(add, pro, Simulator.n1, Simulator.a1, Simulator.b, "l1");
+				if(isHitorMiss){
+					return 0;
+				}else{
+					//boolean isL2HitorMiss = Util.hitOrMiss(add, pro, n, a, b, "l2");
+					
+				}
 		return 0;
 	}
 }
