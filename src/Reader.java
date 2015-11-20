2809b3af78ebfbdd31459bae6ab0e063f67d3a17
Xiaoyu <veronica.xiaoyu@gmail.com>
2015-11-20 16:47:45 -0500

    store

--- src/Reader.java
+++ src/Reader.java
@@ -16,29 +16,30 @@ public class Reader {
 			System.out.println(coreid + ": L1 read hit");
 			return cycle+1;
 		} else {
-			int cycle_needed = readMiss(pro, address, coreid);
+			int cycle_needed = readMiss(address, coreid);
 			return cycle+cycle_needed;
 		}
 	}
 
-	public int readMiss(Processor pro, String add, String coreid){
+	public int readMiss(String add, String coreid){
 		int cycle_used = 0;
 		String homeid = Integer.parseInt(add.substring(19-Simulator.p+1, 20),2) +"";
 		int local2home = Util.getManhattanDistance(coreid,homeid, Simulator.p);
+		Processor pro = Simulator.processorsTable.get(homeid);
 		if(pro.l2.directory.blocktable.contains(add)){
-
+			return cycle_used;
 		}else{
 			// L2 miss, need to fetch from memory
 			// add:
 			// 1. messages to and from controller
 			// 2. memory access delay d1
-			int manhattanDistance = Util.getManhattanDistance(coreid, "0", Simulator.p);
-			cycle_used += 2*manhattanDistance + Simulator.d1;
-			System.out.println(coreid + ": L2 miss, need to send message to controller" +
+			int home2controller = Util.getManhattanDistance(homeid, "0", Simulator.p);
+			System.out.println(coreid + ": L2 uncached, need to send message to controller" +
 					" and fetch data from memory");
+			int store_time = Util.storeBlockToCache(add, "l2",);
+			cycle_used += 2*local2home + 2*home2controller + Simulator.d1;
 			return cycle_used;
 		}
-
 	}
 	
 	public int exclusive () {
