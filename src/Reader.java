54735684fd44cbc59681adbe183ca6d2434e2677
Colin Zhang <me@colinzhang.com>
2015-11-20 21:28:25 -0500

    modify reader and writer

--- src/Reader.java
+++ src/Reader.java
@@ -1,11 +1,10 @@
 
-
 public class Reader {
-	public Reader () {
-		
+	public Reader() {
+
 	}
-	
-	public int run (String coreid, String address, int cycle) {
+
+	public int run(String coreid, String address, int cycle) {
 		Processor pro = (Processor) Simulator.processorsTable.get(coreid);
 		boolean l1readHit = Util.hitOrMiss(address, pro, Simulator.n1, Simulator.a1, Simulator.b);
 		if (l1readHit) {
@@ -14,47 +13,47 @@ public class Reader {
 			return readMiss(address, coreid, cycle);
 		}
 	}
-	
+
 	public int readHit(String address, String coreid, int cycle) {
-		Util.updateLRU (address, coreid, "l1", cycle); 
+		Util.updateLRU(address, coreid, "l1", cycle);
 		return cycle;
 	}
 
-	public int readMiss(String add, String coreid, int cur_cycle){
-		String homeid = Integer.parseInt(add.substring(19-Simulator.p+1, 20),2) +"";
+	public int readMiss(String add, String coreid, int cur_cycle) {
+		String homeid = Integer.parseInt(add.substring(19 - Simulator.p + 1, 20), 2) + "";
 		Processor pro = Simulator.processorsTable.get(homeid);
-		
-		if(pro.l2.directory.blocktable.contains(add)){
+
+		if (pro.l2.directory.blocktable.contains(add)) {
 			// read miss, l2 hit, check state of block
-			if(pro.l2.directory.blocktable.get(add).state == Directory.SHARED_STATE){
+			if (pro.l2.directory.blocktable.get(add).state == Directory.SHARED_STATE) {
 				// read miss, l2 shared
 				cur_cycle = shared(coreid, homeid, add, cur_cycle);
-			}else if (pro.l2.directory.blocktable.get(add).state == Directory.MODIFIED_STATE) {
+			} else if (pro.l2.directory.blocktable.get(add).state == Directory.MODIFIED_STATE) {
 				// read miss, l2 exclusive
 				cur_cycle = exclusive(coreid, homeid, add, cur_cycle);
 			}
-		}else{
+		} else {
 			// read miss, l2 uncached
 			cur_cycle = uncached(coreid, homeid, add, cur_cycle);
 		}
 		return cur_cycle;
 	}
-	
-	public int uncached (String localid, String homeid, String address, int cycle) {
-		int local2home = Util.getManhattanDistance(localid,homeid, Simulator.p);
+
+	public int uncached(String localid, String homeid, String address, int cycle) {
+		int local2home = Util.getManhattanDistance(localid, homeid, Simulator.p);
 		int home2controller = Util.getManhattanDistance(homeid, "0", Simulator.p);
 		Processor processor = Simulator.processorsTable.get(homeid);
-		
+
 		// 1. L sends request to H
 		cycle = cycle + local2home * Simulator.C;
-		
+
 		// 2. H sends request to 0
 		cycle = cycle + home2controller * Simulator.C;
-		
+
 		// 3. 0 get data from mem
 		// return to H
 		cycle = cycle + Simulator.d1 + home2controller * Simulator.C;
-		
+
 		// 4. H get data, return to L
 		// set state of block to "shared" in dir
 		// store to l2
@@ -63,70 +62,67 @@ public class Reader {
 		processor.l2.directory.blocktable.get(address).state = Directory.SHARED_STATE;
 		processor.l2.directory.blocktable.get(address).sharers.add(localid);
 		cycle = cycle + local2home * Simulator.C;
-		
-		
+
 		// L get data
 		// store to l1
 		// set state of block to "shared"
 		cycle = cycle + Util.storeBlockToCache(address, "l1", localid, cycle);
 		Util.setBlockStatus(localid, address, Directory.SHARED_STATE);
-		
+
 		return cycle;
 	}
-	
-	public int shared (String localid, String homeid, String address, int cycle) {
+
+	public int shared(String localid, String homeid, String address, int cycle) {
 		Processor processor = Simulator.processorsTable.get(homeid);
-		
+
 		// 1. L sends request to H
 		int manhattanDistance = Util.getManhattanDistance(localid, homeid, Simulator.p);
 		cycle = manhattanDistance * Simulator.C + cycle;
-		
+
 		// 2. H return block to L
 		// add sharer L
-		Util.updateLRU (address, homeid, "l2", cycle);
+		Util.updateLRU(address, homeid, "l2", cycle);
 		cycle = manhattanDistance * Simulator.C + Simulator.d + cycle;
 		processor.l2.directory.blocktable.get(address).sharers.add(localid);
-		
-		
-		
+
 		// L get data
 		// store to l1
 		// set state of block to "shared"
 		cycle = cycle + Util.storeBlockToCache(address, "l1", localid, cycle);
 		Util.setBlockStatus(localid, address, Directory.SHARED_STATE);
-		
+
 		return cycle;
 	}
-	
-	public int exclusive (String localid, String homeid, String address, int cycle) {
+
+	public int exclusive(String localid, String homeid, String address, int cycle) {
 		Processor processor = Simulator.processorsTable.get(homeid);
-		
+
 		// 1. L sends request to H
 		int manhattanDistance = Util.getManhattanDistance(localid, homeid, Simulator.p);
 		cycle = manhattanDistance * Simulator.C + cycle;
-		
+
 		// 2. H return owner to L
 		cycle = manhattanDistance * Simulator.C + cycle;
-		
+
 		// 3. L get R, sends request to R
 		String remoteid = processor.l2.directory.blocktable.get(address).owner;
 		manhattanDistance = Util.getManhattanDistance(localid, remoteid, Simulator.p);
 		cycle = manhattanDistance * Simulator.C + cycle;
-		
+
 		// 4. R sends block to L and H
 		// set state of block to "shared"
 		Util.setBlockStatus(remoteid, address, Directory.SHARED_STATE);
 		int cycleByL = 0;
 		int cycleByH = 0;
-		cycleByL = Util.getManhattanDistance(localid, remoteid, Simulator.p)*Simulator.C;
-		cycleByH = Util.getManhattanDistance(homeid, remoteid, Simulator.p)*Simulator.C;
-		
+		cycleByL = Util.getManhattanDistance(localid, remoteid, Simulator.p) * Simulator.C;
+		cycleByH = Util.getManhattanDistance(homeid, remoteid, Simulator.p) * Simulator.C;
+
 		// L get block
 		// store to L1
 		// set state of Block to "shared"
 		cycleByL = cycleByL + Util.storeBlockToCache(address, "l1", localid, cycleByL);
 		Util.setBlockStatus(localid, address, Directory.SHARED_STATE);
-		
+
 		// H change to "shared"
 		// store to L2
 		// add sharer
@@ -135,7 +131,7 @@ public class Reader {
 		processor.l2.directory.blocktable.get(address).sharers.clear();
 		processor.l2.directory.blocktable.get(address).sharers.add(remoteid);
 		processor.l2.directory.blocktable.get(address).sharers.add(localid);
-		
+
 		cycle = cycle + Math.max(cycleByL, cycleByH);
 		return cycle;
 	}
--- src/Writer.java
+++ src/Writer.java
@@ -1,164 +1,173 @@
 
 public class Writer {
-	public Writer () {
-		
+	public Writer() {
+
 	}
-	
-	public int run (String add, String coreid, int currentCycle) {
+
+	public int run(String add, String coreid, int cycle) {
 		// run write operation, and return the number of finishing cycle
-		//if the operation is miss or hit
+		// if the operation is miss or hit
 		Processor pro = Simulator.processorsTable.get(coreid);
-		String homeid = Integer.parseInt(add.substring(19-Simulator.p+1, 20),2) +"";
+		String homeid = Integer.parseInt(add.substring(19 - Simulator.p + 1, 20), 2) + "";
 		Processor homeProcessor = Simulator.processorsTable.get(homeid);
 		boolean hit = Util.hitOrMiss(add, pro, Simulator.n1, Simulator.a1, Simulator.b);
-		int manhattanDistance = Util.getManhattanDistance(coreid, homeid, Simulator.p);
-		int cycles = 0;
-		cycles = Simulator.d;
-		if(hit){
-			//L to H
-			cycles = cycles+manhattanDistance * Simulator.C;
-			
-			//change the block's state to exclusive and the owner to L
-			homeProcessor.l2.directory.blocktable.get(add).owner=coreid;
-			homeProcessor.l2.directory.blocktable.get(add).state = 1;
-			//H to L, 
-			cycles = cycles+manhattanDistance * Simulator.C;
-			
-			//the longest latency in all the sharer
-			int longestLatency = 0;
-			for(int i =0;i<homeProcessor.l2.directory.blocktable.get(add).sharers.size();i++){
-				int latency = Util.getManhattanDistance(coreid, homeProcessor.l2.directory.blocktable.get(add).sharers.get(i), Simulator.p);
-				if(latency>longestLatency){
-					longestLatency = latency;
-				}
+
+		if (hit) {
+			int blockStatus = Util.getBlockStatus(coreid, add);
+			if (blockStatus == Directory.MODIFIED_STATE) {
+				// write hit, exclusive
+				return hitExclusive(coreid, add, cycle);
+			} else {
+				// write hit, shared
+				return share(coreid, homeid, add, cycle, hit);
 			}
-			//L to R
-			Util.setBlockStatus(coreid,add,1);// set the block's state to exclusive in local l1
-			cycles = cycles+longestLatency * Simulator.C;
-			
-			longestLatency = 0;
-			for(int i =0;i<homeProcessor.l2.directory.blocktable.get(add).sharers.size();i++){
-				int latency = Util.getManhattanDistance(coreid, homeProcessor.l2.directory.blocktable.get(add).sharers.get(i), Simulator.p);
-				if(latency>longestLatency){
-					longestLatency = latency;
-				}
-				String rn = homeProcessor.l2.directory.blocktable.get(add).sharers.get(i);
-				Processor romteProcessor = Simulator.processorsTable.get(rn);
-				// set remote blocks' state to invalid
-				Util.setBlockStatus(rn,add,1);
-			}
-			// Rs send ack to L
-			cycles = cycles+longestLatency * Simulator.C;
-			//write operation
-			
-		}else{
-			
+		} else {
+
 			if (homeProcessor.l2.directory.blocktable.contains(add)) {
-				if(homeProcessor.l2.directory.blocktable.get(add).state == 0){
-					//if the block's state is uncached
-					//L to H
-					cycles = cycles+manhattanDistance * Simulator.C;
-					//H to P0
-					int hto0manhattanDistance = Util.getManhattanDistance(homeid, "0", Simulator.p);
-					cycles = cycles+hto0manhattanDistance * Simulator.C;
-					//p0 fetch data from memory
-					cycles = cycles+Simulator.d1;
-					//p0 to H
-					cycles = cycles+hto0manhattanDistance * Simulator.C;
-					
-					//store the block 
-					Util.storeBlockToCache(add,"l2",homeid,cycles);
-					//change the block's state to exclusive in home node
-					homeProcessor.l2.directory.blocktable.get(add).state = 1; 
-					//set the block's owner
-					homeProcessor.l2.directory.blocktable.get(add).owner = coreid;
-					//H to L
-					cycles = cycles+manhattanDistance * Simulator.C;
-					//store the block in local l1
-					Util.storeBlockToCache(add,"l1",coreid,cycles);
-					//set the block's state to exclusive in local l1
-					Util.setBlockStatus(coreid,add,1); 
-				}else if(homeProcessor.l2.directory.blocktable.get(add).state == 1){
-					//L to H
-					cycles = cycles+manhattanDistance * Simulator.C;
-					
-					//H to L, 
-					cycles = cycles+manhattanDistance * Simulator.C;
-					
-					//L to R
-					int latency = Util.getManhattanDistance(coreid,homeProcessor.l2.directory.blocktable.get(add).owner, Simulator.p);
-					cycles = cycles+latency * Simulator.C;
-					
-					//set the block's state to invalid in the remote l1
-					Util.setBlockStatus(homeProcessor.l2.directory.blocktable.get(add).owner,add,1);
-					
-					//send block to L, and send alert to H
-					int rToHLatency = Util.getManhattanDistance(homeid,homeProcessor.l2.directory.blocktable.get(add).owner, Simulator.p);
-					
-					if(latency>rToHLatency){
-						cycles = cycles+latency * Simulator.C;
-					}else{
-						cycles = cycles+rToHLatency * Simulator.C;
-					}
-					//store the block in local l1 and set the block's state to exclusive
-					Util.storeBlockToCache(add,"l1",coreid,cycles);
-					Util.setBlockStatus(coreid,add,1);
-					
-					//change the owner of the block in H to L
-					homeProcessor.l2.directory.blocktable.get(add).owner = coreid;
-				}else if(homeProcessor.l2.directory.blocktable.get(add).state == 2){
-					//if the block's state is shared
-					//L to H
-					cycles = cycles+manhattanDistance * Simulator.C;
-					
-					//change the block's state to exclusive and the owner to L
-					homeProcessor.l2.directory.blocktable.get(add).owner=coreid;
-					homeProcessor.l2.directory.blocktable.get(add).state = 1;
-					//H to L, 
-					cycles = cycles+manhattanDistance * Simulator.C;
-					
-					//the longest latency in all the sharer
-					int longestLatency = 0;
-					for(int i =0;i<homeProcessor.l2.directory.blocktable.get(add).sharers.size();i++){
-						int latency = Util.getManhattanDistance(coreid, homeProcessor.l2.directory.blocktable.get(add).sharers.get(i), Simulator.p);
-						if(latency>longestLatency){
-							longestLatency = latency;
-						}
-					}
-					// set the block's state to exclusive in local l1
-					Util.setBlockStatus(coreid,add,1);
-					
-					//store the block in local l1
-					Util.storeBlockToCache(add,"l1",coreid,cycles);
-					//L to R
-					cycles = cycles+longestLatency * Simulator.C;
-					
-					longestLatency = 0;
-					for(int i =0;i<homeProcessor.l2.directory.blocktable.get(add).sharers.size();i++){
-						int latency = Util.getManhattanDistance(coreid, homeProcessor.l2.directory.blocktable.get(add).sharers.get(i), Simulator.p);
-						if(latency>longestLatency){
-							longestLatency = latency;
-						}
-						String rn = homeProcessor.l2.directory.blocktable.get(add).sharers.get(i);
-						Processor romteProcessor = Simulator.processorsTable.get(rn);
-						//set remote blocks' state to invalid in l1
-						Util.setBlockStatus(rn,add,0);
-					}
-					// Rs send ack to L
-					cycles = cycles+longestLatency * Simulator.C;
-					//write operation
+				// write miss, l2 hit
+				if (homeProcessor.l2.directory.blocktable.get(add).state == Directory.MODIFIED_STATE) {
+					// write miss, l2 exclusive
+					return missExclusive(coreid, homeid, add, cycle);
+				} else {
+					// write miss, l2 shared
+					return share(coreid, homeid, add, cycle, hit);
 				}
 			} else {
-						//l2 does not have the block
-						
+				// write miss, l2 uncached
+				return uncached(coreid, homeid, add, cycle);
 			}
 		}
-		return cycles;
 	}
-	
+
 	public int hitExclusive(String localid, String address, int cycle) {
-		Util.updateLRU (address, localid, "l1", cycle); 
+		Util.updateLRU(address, localid, "l1", cycle);
 		return cycle;
 	}
-	
+
+	public int uncached(String localid, String homeid, String address, int cycle) {
+		int local2home = Util.getManhattanDistance(localid, homeid, Simulator.p);
+		int home2controller = Util.getManhattanDistance(homeid, "0", Simulator.p);
+		Processor processor = Simulator.processorsTable.get(homeid);
+
+		// 1. L sends request to H
+		cycle = cycle + local2home * Simulator.C;
+
+		// 2. H sends request to 0
+		cycle = cycle + home2controller * Simulator.C;
+
+		// 3. 0 get data from mem
+		// return to H
+		cycle = cycle + Simulator.d1 + home2controller * Simulator.C;
+
+		// 4. H get data, return to L
+		// set state of block to "exclusive" in dir
+		// store to l2
+		// add sharer L
+		cycle = cycle + Util.storeBlockToCache(address, "l2", homeid, cycle);
+		processor.l2.directory.blocktable.get(address).state = Directory.MODIFIED_STATE;
+		processor.l2.directory.blocktable.get(address).sharers.add(localid);
+		cycle = cycle + local2home * Simulator.C;
+
+		// L get data
+		// store to l1
+		// set state of block to "exclusive"
+		cycle = cycle + Util.storeBlockToCache(address, "l1", localid, cycle);
+		Util.setBlockStatus(localid, address, Directory.MODIFIED_STATE);
+
+		return cycle;
+	}
+
+	public int missExclusive(String localid, String homeid, String address, int cycle) {
+		Processor processor = Simulator.processorsTable.get(homeid);
+
+		// 1. L sends request to H
+		int manhattanDistance = Util.getManhattanDistance(localid, homeid, Simulator.p);
+		cycle = manhattanDistance * Simulator.C + cycle;
+
+		// 2. H return owner to L
+		cycle = manhattanDistance * Simulator.C + cycle;
+
+		// 3. L get R, sends request to R
+		String remoteid = processor.l2.directory.blocktable.get(address).owner;
+		manhattanDistance = Util.getManhattanDistance(localid, remoteid, Simulator.p);
+		cycle = manhattanDistance * Simulator.C + cycle;
+
+		// 4. R sends block to L and H
+		// set state of block to "invalid"
+		Util.setBlockStatus(remoteid, address, Directory.INVALID_STATE);
+		int cycleByL = 0;
+		int cycleByH = 0;
+		cycleByL = Util.getManhattanDistance(localid, remoteid, Simulator.p) * Simulator.C;
+		cycleByH = Util.getManhattanDistance(homeid, remoteid, Simulator.p) * Simulator.C;
+
+		// L get block
+		// store to L1
+		// set state of Block to "exclusive"
+		cycleByL = cycleByL + Util.storeBlockToCache(address, "l1", localid, cycleByL);
+		Util.setBlockStatus(localid, address, Directory.MODIFIED_STATE);
+
+		// L get data perform write
+		Util.updateLRU(address, localid, "l1", cycle + cycleByL);
+
+		// H change to "exclusive"
+		// store to L2
+		// add sharer
+		processor.l2.directory.blocktable.get(address).state = Directory.MODIFIED_STATE;
+		Util.updateLRU(address, homeid, "l2", cycle + cycleByH);
+		processor.l2.directory.blocktable.get(address).owner = localid;
+
+		cycle = cycle + Math.max(cycleByL, cycleByH);
+		return cycle;
+	}
+
+	public int share(String localid, String homeid, String address, int cycle, boolean hit) {
+		Processor Processor = Simulator.processorsTable.get(homeid);
+		int manhattanDistance = Util.getManhattanDistance(localid, homeid, Simulator.p);
+		// 1. L sends request to H
+		cycle = cycle + manhattanDistance * Simulator.C;
+
+		// 2. H return sharers list to L.
+		// set block state to "exclusive"
+		// change owner to L
+		Util.updateLRU(address, homeid, "l2", cycle);
+		Processor.l2.directory.blocktable.get(address).owner = localid;
+		Processor.l2.directory.blocktable.get(address).state = Directory.MODIFIED_STATE;
+		if (hit) {
+			// return a small message
+		} else {
+			// return a large message
+		}
+		cycle = cycle + manhattanDistance * Simulator.C;
+
+		// 3. L sends invalidating message to sharers
+		// set state to exclusive
+		if (!hit) {
+			cycle = cycle + Util.storeBlockToCache(address, "l1", localid, cycle);
+		}
+		Util.setBlockStatus(localid, address, Directory.MODIFIED_STATE);
+		int longestLatency = 0;
+		for (int i = 0; i < Processor.l2.directory.blocktable.get(address).sharers.size(); i++) {
+			int latency = Util.getManhattanDistance(localid,
+					Processor.l2.directory.blocktable.get(address).sharers.get(i), Simulator.p);
+			if (latency > longestLatency) {
+				longestLatency = latency;
+			}
+		}
+		cycle = cycle + longestLatency * Simulator.C;
+
+		// 4. R sets block state to "invalid"
+		// send ack to L
+		for (int i = 0; i < Processor.l2.directory.blocktable.get(address).sharers.size(); i++) {
+			String rn = Processor.l2.directory.blocktable.get(address).sharers.get(i);
+			// set remote blocks' state to invalid
+			Util.setBlockStatus(rn, address, Directory.INVALID_STATE);
+		}
+		cycle = cycle + longestLatency * Simulator.C;
+
+		// L get ack, perform write
+		Util.updateLRU(address, localid, "l1", cycle);
+
+		return cycle;
+	}
+
 }
